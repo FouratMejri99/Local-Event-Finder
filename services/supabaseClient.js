610683e812b8b1329d7428b3ffeb114327750afe
Fourat Mejri <fouratm3@gmail.com>
2025-11-01 13:10:52 +0100

    changes

--- services/supabaseClient.js
+++ services/supabaseClient.js
@@ -1,8 +1,5 @@
-// services/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-// Replace with your project URL and anon key
-const SUPABASE_URL = "https://YOUR_PROJECT_URL.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_ANON_KEY";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseUrl = "https://zjrtbcewxrzfqoaixpba.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY;
+export const supabase = createClient(supabaseUrl, supabaseKey);
